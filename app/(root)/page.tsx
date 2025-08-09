6fb57a04bd6e787e1ca21b11c06080332ccce274
Djuhar <djuharghm@gmail.com>
2025-08-09 23:25:55 +0700

    pink-container update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,12 @@ export default function Home() {
         <>
             <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
                 <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
-                    Pitch your idea to the world and connect with entrepreneurs
+                    Pitch your ideas and connect with entrepreneurs
                 </h1>
+
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+                </p>
             </section>
         </>
     )
