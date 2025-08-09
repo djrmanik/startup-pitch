d26a610f5ed541fefa10b7739d76571acebf6409
Djuhar <djuharghm@gmail.com>
2025-08-09 22:50:45 +0700

    pink-container

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
     return (
         <>
-            <h1 className="heading">Home</h1>
+            <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+                <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+                    Pitch your idea to the world and connect with entrepreneurs
+                </h1>
+            </section>
         </>
     )
 }
