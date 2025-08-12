453d1233e1059d4b5dc9df1785f37502730be0bd
Djuhar <djuharghm@gmail.com>
2025-08-12 21:45:32 +0700

    cache & live api with next.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
 import SearchForm from "@/components/SearchForm"
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+// import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 
 export default async function Home( {searchParams}: {searchParams: Promise<{query?: string}>}) {
     const query = (await searchParams).query;
 
-    const posts = await client.fetch(STARTUPS_QUERY);
+    // const posts = await client.fetch(STARTUPS_QUERY);
+
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
 
     // const posts = [{
@@ -52,6 +55,8 @@ export default async function Home( {searchParams}: {searchParams: Promise<{quer
                     )}
                 </ul>
             </section>
+            
+            <SanityLive />
         </>
     )
 }
