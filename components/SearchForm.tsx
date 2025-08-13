970021a5255f0413fbdd97c814dda77348c41c1e
Djuhar <djuharghm@gmail.com>
2025-08-13 12:50:17 +0700

    update button search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -21,7 +21,7 @@ const SearchForm = ( { query }: { query?: string } ) => {
                 {query && <SearchFormReset />}
 
                 <button type="submit" className="size-[50px] rounded-full bg-black flex justify-center items-center !important text-white"> 
-                    <Search className="w-10 h-10 shrink-0"/>
+                    <Search className="w-5 h-5"/>
                 </button>
             </div>
         </Form>
--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -13,7 +13,7 @@ const SearchFormReset = () => {
     return (
         <button type="reset" onClick={reset}>
             <Link href="/" className="size-[50px] rounded-full bg-black flex justify-center items-center !important text-white">
-                <X className="w-10 h-10 shrink-0"/>
+                <X className="w-5 h-5"/>
             </Link>
         </button>
     )
